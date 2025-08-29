c5e735e42ef3bb5399a3309ee509567c459cfab1
BacaniLeon <leonbaani@gmail.com>
2025-08-29 12:18:01 +0200

    Updates

--- data/articles.js
+++ data/articles.js
@@ -11,7 +11,7 @@ export const articles = [
     video: "https://www.youtube.com/embed/MOWSNQkO1Yw"
   },
 
- {
+    {
     title: "Interview with our founder",
     date: "27.8.2025",
     calendar: "../resources/images/calendar.png",
@@ -19,83 +19,104 @@ export const articles = [
     paragraf:
      'Our Founder and CEO Arezoo Assarian was interviewed last week by Inspenet TV. We are so proud of our CEO and we hope you check out the interview! <br> <a href="https://inspenet.com/video-tv/recubrimientos-inteligentes-industriales" target="_blank">Watch Interview</a>',
     photo: "../resources/images/interview.jpg",
+    thumbnail: "",
     video: ""
   },
   
    {
-    title: "Interview with our founder",
-    date: "27.8.2025",
+    title: "Test of video news article",
+    date: "29.8.2025",
     calendar: "../resources/images/calendar.png",
-    preview_paragraf: "Our Founder and CEO Arezoo Assarian was interviewed last week by Inspenet TV...",
+    preview_paragraf: "This news article contains thumbnail of the video as picture on preview article...",
     paragraf:
-     'Our Founder and CEO Arezoo Assarian was interviewed last week by Inspenet TV. We are so proud of our CEO and we hope you check out the interview! <br> <a href="https://inspenet.com/video-tv/recubrimientos-inteligentes-industriales" target="_blank">Watch Interview</a>',
-    photo: "../resources/images/interview.jpg",
+     'This news article contains thumbnail of the video as picture on preview article and when you open the entire article it will have the video displayed instead of the thumbnail or photo on top',
+    photo: "../resources/images/about_us.jpg",
+    thumbnail: "",
     video: ""
   },
   
    {
-    title: "Interview with our founder",
-    date: "27.8.2025",
+    title: "Test of video news article",
+    date: "29.8.2025",
     calendar: "../resources/images/calendar.png",
-    preview_paragraf: "Our Founder and CEO Arezoo Assarian was interviewed last week by Inspenet TV...",
+    preview_paragraf: "This news article contains thumbnail of the video as picture on preview article...",
     paragraf:
-     'Our Founder and CEO Arezoo Assarian was interviewed last week by Inspenet TV. We are so proud of our CEO and we hope you check out the interview! <br> <a href="https://inspenet.com/video-tv/recubrimientos-inteligentes-industriales" target="_blank">Watch Interview</a>',
-    photo: "../resources/images/interview.jpg",
+     'This news article contains thumbnail of the video as picture on preview article and when you open the entire article it will have the video displayed instead of the thumbnail or photo on top',
+    photo: "../resources/images/brush.jpg",
+    thumbnail: "",
     video: ""
   },
   
    {
-    title: "Interview with our founder",
-    date: "27.8.2025",
+    title: "Test of video news article",
+    date: "29.8.2025",
     calendar: "../resources/images/calendar.png",
-    preview_paragraf: "Our Founder and CEO Arezoo Assarian was interviewed last week by Inspenet TV...",
+    preview_paragraf: "This news article contains thumbnail of the video as picture on preview article...",
     paragraf:
-     'Our Founder and CEO Arezoo Assarian was interviewed last week by Inspenet TV. We are so proud of our CEO and we hope you check out the interview! <br> <a href="https://inspenet.com/video-tv/recubrimientos-inteligentes-industriales" target="_blank">Watch Interview</a>',
-    photo: "../resources/images/interview.jpg",
+     'This news article contains thumbnail of the video as picture on preview article and when you open the entire article it will have the video displayed instead of the thumbnail or photo on top',
+    photo: "../resources/images/post_sale.jpg",
+    thumbnail: "",
     video: ""
   },
   
    {
-    title: "Interview with our founder",
-    date: "27.8.2025",
+    title: "Test of video news article",
+    date: "29.8.2025",
     calendar: "../resources/images/calendar.png",
-    preview_paragraf: "Our Founder and CEO Arezoo Assarian was interviewed last week by Inspenet TV...",
+    preview_paragraf: "This news article contains thumbnail of the video as picture on preview article...",
     paragraf:
-     'Our Founder and CEO Arezoo Assarian was interviewed last week by Inspenet TV. We are so proud of our CEO and we hope you check out the interview! <br> <a href="https://inspenet.com/video-tv/recubrimientos-inteligentes-industriales" target="_blank">Watch Interview</a>',
-    photo: "../resources/images/interview.jpg",
+     'This news article contains thumbnail of the video as picture on preview article and when you open the entire article it will have the video displayed instead of the thumbnail or photo on top',
+    photo: "../resources/images/inspection.jpg",
+    thumbnail: "",
     video: ""
   },
   
    {
-    title: "Interview with our founder",
-    date: "27.8.2025",
+    title: "Test of video news article",
+    date: "29.8.2025",
     calendar: "../resources/images/calendar.png",
-    preview_paragraf: "Our Founder and CEO Arezoo Assarian was interviewed last week by Inspenet TV...",
+    preview_paragraf: "This news article contains thumbnail of the video as picture on preview article...",
     paragraf:
-     'Our Founder and CEO Arezoo Assarian was interviewed last week by Inspenet TV. We are so proud of our CEO and we hope you check out the interview! <br> <a href="https://inspenet.com/video-tv/recubrimientos-inteligentes-industriales" target="_blank">Watch Interview</a>',
+     'This news article contains thumbnail of the video as picture on preview article and when you open the entire article it will have the video displayed instead of the thumbnail or photo on top',
     photo: "../resources/images/interview.jpg",
+    thumbnail: "",
     video: ""
   },
   
    {
-    title: "Interview with our founder",
-    date: "27.8.2025",
+    title: "Test of video news article",
+    date: "29.8.2025",
     calendar: "../resources/images/calendar.png",
-    preview_paragraf: "Our Founder and CEO Arezoo Assarian was interviewed last week by Inspenet TV...",
+    preview_paragraf: "This news article contains thumbnail of the video as picture on preview article...",
     paragraf:
-     'Our Founder and CEO Arezoo Assarian was interviewed last week by Inspenet TV. We are so proud of our CEO and we hope you check out the interview! <br> <a href="https://inspenet.com/video-tv/recubrimientos-inteligentes-industriales" target="_blank">Watch Interview</a>',
+     'This news article contains thumbnail of the video as picture on preview article and when you open the entire article it will have the video displayed instead of the thumbnail or photo on top',
     photo: "../resources/images/interview.jpg",
+    thumbnail: "",
     video: ""
   },
   
    {
-    title: "Interview with our founder",
-    date: "27.8.2025",
+    title: "Test of video news article",
+    date: "29.8.2025",
     calendar: "../resources/images/calendar.png",
-    preview_paragraf: "Our Founder and CEO Arezoo Assarian was interviewed last week by Inspenet TV...",
+    preview_paragraf: "This news article contains thumbnail of the video as picture on preview article...",
     paragraf:
-     'Our Founder and CEO Arezoo Assarian was interviewed last week by Inspenet TV. We are so proud of our CEO and we hope you check out the interview! <br> <a href="https://inspenet.com/video-tv/recubrimientos-inteligentes-industriales" target="_blank">Watch Interview</a>',
+     'This news article contains thumbnail of the video as picture on preview article and when you open the entire article it will have the video displayed instead of the thumbnail or photo on top',
     photo: "../resources/images/interview.jpg",
+    thumbnail: "",
     video: ""
   },
+  
+   {
+    title: "Test of video news article",
+    date: "29.8.2025",
+    calendar: "../resources/images/calendar.png",
+    preview_paragraf: "This news article contains thumbnail of the video as picture on preview article...",
+    paragraf:
+     'This news article contains thumbnail of the video as picture on preview article and when you open the entire article it will have the video displayed instead of the thumbnail or photo on top',
+    photo: "../resources/images/interview.jpg",
+    thumbnail: "",
+    video: ""
+  },
+  
 ]
