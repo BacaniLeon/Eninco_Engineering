85f958079169295a2b2f286dfe75ceabe3ac7dd8
BacaniLeon <leonbaani@gmail.com>
2025-08-30 01:07:48 +0200

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,12 +1,11 @@
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt';
+</script>
+
 <template>
+  <Analytics />
   <div>
     <NuxtPage/>
   </div>
 </template>
 
-<script setup>
-import { inject } from '@vercel/analytics';
-
-inject();
-
-</script>
